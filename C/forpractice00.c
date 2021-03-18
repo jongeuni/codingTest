c8e4472762380011ba08568c71bdde2edbbe44e9
joungeun <lje0818ovo@naver.com>
2021-03-18 19:57:41 +0900

    for practice
    Long time no see C language

---
+++ C/forpractice00.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// 오른쪽 위 직각삼각형 만들기
+
+int main()
+{
+    int array[5][5] = {0};
+    int count = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = i; j < 5; j++) {
+            count++;
+            array[i][j] = count;
+           
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%d   ",array[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
